b5dfea158934a96b8f751797815c2abafb2fae0f
jiatian2 <173877668@qq.com>
2017-03-14 09:01:44 +0800

    这是第六天的

--- js/shangcheng.js
+++ js/shangcheng.js
@@ -1,6 +1,15 @@
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
+var prev1 = document.getElementById("prev1");
+var next1 = document.getElementById("next1");
+var prev2 = document.getElementById("prev2");
+var next2 = document.getElementById("next2");
+var prev3 = document.getElementById("prev3");
+var next3 = document.getElementById("next3");
 var c3_a = document.getElementsByClassName("c3_a")[0];
+var Oc3_a = document.getElementsByClassName("c3_a")[1];
+var _c3_a = document.getElementsByClassName("c3_a")[2];
+var oc3_a = document.getElementsByClassName("c3_a")[3];
 var _bottom = document.getElementsByClassName("bottomline")[0];
 var oItem = document.getElementsByClassName("item")[0];
 //console.log(c3_a);
@@ -9,8 +18,39 @@ next .onclick = function(){
 //	c3_a.style.background = "red";
 //	console.log(c3_a);
 }
+
 prev .onclick = function(){
 	c3_a.style.left = 0+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+next1 .onclick = function(){
+	Oc3_a.style.left = -1110+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+prev1 .onclick = function(){
+	Oc3_a.style.left = 0+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+next2 .onclick = function(){
+	_c3_a.style.left = -1110+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+prev2 .onclick = function(){
+	_c3_a.style.left = 0+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+next3 .onclick = function(){
+	oc3_a.style.left = -1110+"px";
+//	c3_a.style.background = "red";
+//	console.log(c3_a);
+}
+prev3 .onclick = function(){
+	oc3_a.style.left = 0+"px";
 //	c3_a.style.background = "red";
 //	console.log(c3_a);
 }
